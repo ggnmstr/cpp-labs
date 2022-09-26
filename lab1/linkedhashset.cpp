c0e29c61aeeeba141d144e47da2ce5ffa4a7eddb
Jora Babayan <jora.babayan@gmail.com>
2022-09-26 12:16:12 -0400

    started writing insert func

--- lab1/linkedhashset.cpp
+++ lab1/linkedhashset.cpp
@@ -1,13 +1,19 @@
 #include "linkedhashset.hpp"
 
-linkedhs::linkedhs() {
-    arr_ = new std::list<student>[512];
+linkedhs::linkedhs():
+    capacity_(512), 
+    arr_(new std::list<student>[capacity_]) {
+
 }
 
 linkedhs::~linkedhs() {
     delete[] arr_;
 }
 
-bool linkedhs::insert(const int &e) {
-
+bool linkedhs::insert(const element &e) {
+    long long hash = e.hash();
+    hash %= capacity_;
+    (arr_[hash]).push_back(e);
+    return 0;
+    //arr_[]
 }
--- lab1/linkedhashset.hpp
+++ lab1/linkedhashset.hpp
@@ -2,7 +2,7 @@
 
 #include <list>
 #include "student.hpp"
-
+typedef student element;
 class linkedhs {
 public:
     linkedhs();
@@ -13,4 +13,5 @@ public:
 
 private:
     std::list <student> *arr_;
+    int capacity_;
 };
