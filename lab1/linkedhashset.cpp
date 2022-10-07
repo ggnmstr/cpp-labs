81256b6f6df4ed601f34bd8c02c1d10637799ec1
Jora Babayan <jora.babayan@gmail.com>
2022-10-07 12:30:56 -0400

    fixed segfault. added size(), isempty(), started working on copyconstructor

--- lab1/linkedhashset.cpp
+++ lab1/linkedhashset.cpp
@@ -1,8 +1,25 @@
 #include "linkedhashset.hpp"
 
 linkedhs::linkedhs():
-    capacity_(512),
+    capacity_(DEFAULT_CAPACITY),
+    size_(0),
     arr_(new std::list<student>*[capacity_]()) {
+}
+
+linkedhs::linkedhs(const linkedhs &other):
+    capacity_(other.capacity_),
+    size_(other.size_),
+    arr_(new std::list<student>*[capacity_]()){
+        for (int i = 0; i < capacity_; i++){
+            if (other.arr_[i]){
+                arr_[i] = new std::list<student>;
+                for (auto it = other.arr_[i]->begin(); it != other.arr_[i]->end();it++){
+                    student s(*it);
+                    this->arr_[i]->push_back(s);
+                }
+            }
+        }
+
 }
 
 linkedhs::~linkedhs() {
@@ -12,13 +29,40 @@ linkedhs::~linkedhs() {
     delete[] arr_;
 }
 
+size_t linkedhs::size() const{
+    return size_;
+}
+
+bool linkedhs::empty() const{
+    return size_==0;
+}
+
 bool linkedhs::insert(const element &e) {
     unsigned long long hash = e.hash();
     hash %= capacity_;
     if (arr_[hash] == nullptr){
         arr_[hash] = new std::list<student>;
     }
-    arr_[hash]->push_back(e);
-    return 0;
+    std::list<student> &s1 = *arr_[hash];
+    s1.push_back(e);
+    size_++;
+    return true;
     //arr_[]
+}
+
+bool linkedhs::remove(const element &e) {
+    unsigned long long hash = e.hash();
+    hash %= capacity_;
+    std::list<student> *list = arr_[hash];
+    
+    
+    for (auto it = list->begin(); it != list->end(); ++it){
+        if (e == *it){
+            list->erase(it);
+            size_--;
+            return true;
+        }
+    }
+    return false;
+    
 }
--- lab1/linkedhashset.hpp
+++ lab1/linkedhashset.hpp
@@ -6,12 +6,19 @@ typedef student element;
 class linkedhs {
 public:
     linkedhs();
+    linkedhs(const linkedhs &other);
     ~linkedhs();
+
+    size_t size() const;
+    bool empty() const;
+
 
     bool insert(const element &e);
     bool remove(const element &e);
 
 private:
+    static const int DEFAULT_CAPACITY = 512;
     int capacity_;
+    size_t size_;
     std::list<student> **arr_;
 };
