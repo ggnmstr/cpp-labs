37d947f01776ee55fda8faf313c2e8a29c5dd6d6
George Babayan <jora.babayan@gmail.com>
2022-10-15 20:16:37 +0700

    started working on iterator & minor fixes

--- lab1/linkedhashset.cpp
+++ lab1/linkedhashset.cpp
@@ -1,39 +1,38 @@
 #include "linkedhashset.hpp"
 
-linkedhs::linkedhs():
-    capacity_(DEFAULT_CAPACITY),
-    size_(0),
-    arr_(new std::list<student>*[capacity_]()) {
+linkedhs::linkedhs() :
+        capacity_(DEFAULT_CAPACITY),
+        size_(0),
+        arr_(new std::list<student> *[capacity_]()) {
 }
 
-linkedhs::linkedhs(const linkedhs &other):
-    capacity_(other.capacity_),
-    size_(other.size_),
-    arr_(new std::list<student>*[capacity_]()){
-        for (int i = 0; i < capacity_; i++){
-            if (other.arr_[i]){
-                arr_[i] = new std::list<student>;
-                for (auto it = other.arr_[i]->begin(); it != other.arr_[i]->end();it++){
-                    student s(*it);
-                    this->arr_[i]->push_back(s);
-                }
+linkedhs::linkedhs(const linkedhs &other) :
+        capacity_(other.capacity_),
+        size_(other.size_),
+        arr_(new std::list<student> *[capacity_]()) {
+    for (int i = 0; i < capacity_; i++) {
+        if (other.arr_[i]) {
+            arr_[i] = new std::list<student>;
+            for (auto it = other.arr_[i]->begin(); it != other.arr_[i]->end(); it++) {
+                student s(*it);
+                this->arr_[i]->push_back(s);
             }
         }
-
+    }
 }
 
-linkedhs& linkedhs::operator=(const linkedhs &other){
+linkedhs &linkedhs::operator=(const linkedhs &other) {
     this->clear();
-    if (other.capacity_ != capacity_){
+    if (other.capacity_ != capacity_) {
         delete[] arr_;
         capacity_ = other.capacity_;
-        arr_ = new std::list<student>*[capacity_]();
+        arr_ = new std::list<student> *[capacity_]();
     }
     size_ = other.size_;
-    for (int i = 0; i < other.capacity_; i++){
-        if (other.arr_[i]){
+    for (int i = 0; i < other.capacity_; i++) {
+        if (other.arr_[i]) {
             arr_[i] = new std::list<student>;
-            for (auto it = other.arr_[i]->begin(); it != other.arr_[i]->end();it++){
+            for (auto it = other.arr_[i]->begin(); it != other.arr_[i]->end(); it++) {
                 student s(*it);
                 this->arr_[i]->push_back(s);
             }
@@ -47,7 +46,7 @@ linkedhs::~linkedhs() {
     delete[] arr_;
 }
 
-void linkedhs::swap(linkedhs &other){
+void linkedhs::swap(linkedhs &other) {
     size_t tmp = other.size_;
     other.size_ = size_;
     size_ = tmp;
@@ -62,19 +61,19 @@ void linkedhs::swap(linkedhs &other){
 }
 
 
-size_t linkedhs::size() const{
+size_t linkedhs::size() const {
     return size_;
 }
 
-bool linkedhs::empty() const{
-    return size_==0;
+bool linkedhs::empty() const {
+    return size_ == 0;
 }
 
-bool linkedhs::contains(const element &e) const{
+bool linkedhs::contains(const element &e) const {
     unsigned long long hash = e.hash();
     if (arr_[hash] == nullptr) return false;
     std::list<student> &list = *arr_[hash];
-    for (student &x : list){
+    for (student &x: list) {
         if (x == e) return true;
     }
     return false;
@@ -83,36 +82,39 @@ bool linkedhs::contains(const element &e) const{
 bool linkedhs::insert(const element &e) {
     unsigned long long hash = e.hash();
     hash %= capacity_;
-    if (arr_[hash] == nullptr){
+    if (arr_[hash] == nullptr) {
         arr_[hash] = new std::list<student>;
     }
     std::list<student> &s1 = *arr_[hash];
+    //const student ecopy(e);
     s1.push_back(e);
     size_++;
+    //const student *p = &e;
+    //added_.push_back(p);
     return true;
-    //arr_[]
 }
 
 bool linkedhs::remove(const element &e) {
     unsigned long long hash = e.hash();
     hash %= capacity_;
     std::list<student> *list = arr_[hash];
-    
-    
-    for (auto it = list->begin(); it != list->end(); ++it){
-        if (e == *it){
+
+
+    for (auto it = list->begin(); it != list->end(); ++it) {
+        if (e == *it) {
             list->erase(it);
             size_--;
             return true;
         }
     }
     return false;
-    
+
 }
 
-bool linkedhs::operator==(const linkedhs &other) const{
+bool linkedhs::operator==(const linkedhs &other) const {
     if (capacity_ != other.capacity_ || size_ != other.size_) return false;
-    for (int i = 0; i < capacity_; i++){
+    //todo : rewrite the following code so it only uses iterator and contains()
+    for (int i = 0; i < capacity_; i++) {
         if (arr_[i] != nullptr && other.arr_[i] == nullptr) return false;
         if (arr_[i] == nullptr && other.arr_[i] == nullptr) continue;
         std::list<student> &tlist = *arr_[i];
@@ -122,13 +124,13 @@ bool linkedhs::operator==(const linkedhs &other) const{
     return true;
 }
 
-bool linkedhs::operator!=(const linkedhs &other) const{
+bool linkedhs::operator!=(const linkedhs &other) const {
     return !(*this == other);
 }
 
 
-void linkedhs::clear(){
-    for (int i = 0; i < capacity_ ; i++){
+void linkedhs::clear() {
+    for (int i = 0; i < capacity_; i++) {
         delete arr_[i];
     }
 }
--- lab1/linkedhashset.hpp
+++ lab1/linkedhashset.hpp
@@ -2,27 +2,46 @@
 
 #include <list>
 #include "student.hpp"
+
 typedef student element;
+
 class linkedhs {
 public:
+    class iterator {
+    public:
+        element operator*();
+        iterator operator++(int);
+        iterator operator--();
+        bool operator==(const iterator &other) const;
+        bool operator!=(const iterator &other) const;
+    private:
+
+    };
+
+
     linkedhs();
+
     linkedhs(const linkedhs &other);
+
     ~linkedhs();
-    linkedhs & operator=(const linkedhs & other);
+
+    linkedhs &operator=(const linkedhs &other);
 
     bool insert(const element &e);
+
     bool remove(const element &e);
 
     void swap(linkedhs &other);
 
     size_t size() const;
+
     bool empty() const;
 
-    bool contains(const element & e) const;
+    bool contains(const element &e) const;
 
-    bool operator==(const linkedhs & other) const;
-    bool operator!=(const linkedhs & other) const;
+    bool operator==(const linkedhs &other) const;
 
+    bool operator!=(const linkedhs &other) const;
 
 
     void clear();
@@ -32,4 +51,5 @@ private:
     size_t capacity_;
     size_t size_;
     std::list<student> **arr_;
+    //std::list<student*> added_;
 };
