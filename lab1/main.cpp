ac80c04dda0daf897b30b6f25c44139467f37b4f
Jora Babayan <jora.babayan@gmail.com>
2022-10-17 23:46:28 -0400

    update main.cpp (test)

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -8,13 +8,36 @@ int main(){
     student a2(78,"Abrik");
     student a2c(78,"Abrik");
     student a3(22,"Anya");
-    linkedhs l1; 
+    student a3c(22,"Anya");
+    student a4(20,"Misha");
+    student a5(22, "Katya");
+    student a6(16,"Gennady");
+    linkedhs l1;
     linkedhs l2;
-    // order of added elements effects opertor==, that's incorrect
-    // should work on std::list<student> == std::list<student>
+
     l1.insert(a1);
-    l1.remove(a1);
-    l2.insert(a2c);
-    l2.insert(a1c);
+    l1.insert(a2);
+    l1.insert(a3);
+    l2.insert(a4);
+    l2.insert(a5);
+    //std::cout << l1.contains(a3) << std::endl;
+    //std::cout << l2.contains(a5) << std::endl;
+    //l1.swap(l2);
+    //std::cout << l1.contains(a3) << std::endl;
+    //std::cout << l2.contains(a5) << std::endl;
+    //std::cout << l1.contains(a5) << std::endl;
+    //std::cout << l2.contains(a3) << std::endl;
+    linkedhs l3(l2);
+    std::cout << l3.contains(a4) << std::endl;
+    std::cout << l3.contains(a5) << std::endl;
+    l2.remove(a4);
+    l3.remove(a5);
+    std::cout << l3.contains(a4) << std::endl;
+    std::cout << l3.contains(a5) << std::endl;
+    //l3 = l2;
+    l1.clear();
+    //std::cout << l3.contains(a4) << std::endl;
+    //std::cout << l3.contains(a5) << std::endl;
+
     return 0;
 }
